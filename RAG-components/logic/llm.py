4ec976564a47ab0ef2b9b8d2e1c414076cbb3eda
fedor <shadaevf@mail.ru>
2025-05-21 21:54:52 +0300

    Rag (V0.1)

---
+++ RAG-components/logic/llm.py
@@ -0,0 +1,17 @@
+from dotenv import dotenv_values
+from langchain_gigachat import GigaChat
+from langchain_core.language_models import BaseChatModel
+
+
+
+def get_model(model_name: str="GigaChat-2") -> BaseChatModel:
+
+    config = dotenv_values(".env")
+
+    giga = GigaChat(
+    credentials=config["GIGACHAT_AUTHORIZATION_KEY"],
+    verify_ssl_certs=False,
+    scope="GIGACHAT_API_PERS",
+    model=model_name
+    )
+    return giga
---
+++ RAG-components/models/pydantic_models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+
+class GradeDocuments(BaseModel):
+    """Grade documents using a binary score for relevance check."""
+
+    binary_score: str = Field(
+        description="Relevance score: 'yes' if relevant, or 'no' if not relevant"
+    )
